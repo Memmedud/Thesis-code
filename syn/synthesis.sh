596501de7a60a90ae26d4419de3bffbc2b4b474a
Memmedud <mathst23@gmail.com>
2024-01-06 14:20:57 +0100

    Add more synthesis scripts

--- syn/synthesis.sh
+++ syn/synthesis.sh
@@ -1,1 +1,16 @@
 #Plug into ibex and vicuna scripts...
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+IBEX_DIR=$SCRIPT_DIR/../hw/ibex
+
+# Expand with Vicuna and ibex_pext as well
+
+export sv2v=$SCRIPT_DIR/sv2v
+export sta=$SCRIPT_DIR/OpenSTA
+export PATH=$sv2v/bin:$PATH
+export PATH=$sta/app:$PATH
+
+# Remember to set up syn_setup.sh in ibex syn folder
+$IBEX_DIR/syn/syn_yosys.sh
+
+# Do something with results...
