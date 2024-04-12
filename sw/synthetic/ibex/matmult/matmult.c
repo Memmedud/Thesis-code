bef07ddc283920fa79a7ab16f08961bb9d6a9992
Memmedud <martimst@stud.ntnu.no>
2024-04-12 15:07:56 +0200

    Optimize matmult benchmark

--- sw/synthetic/ibex/matmult/matmult.c
+++ sw/synthetic/ibex/matmult/matmult.c
@@ -7,15 +7,15 @@
 #include  <stdlib.h>
 
 //#define USE_PEXT
-#define WIDTH8
+#define WIDTH32
 
 #if defined(USE_PEXT)
 #include <rvp_intrinsic.h>
 #endif
 
-#define n_len 8
-#define m_len 8
-#define p_len 8
+#define n_len 128
+#define m_len 128
+#define p_len 128
 
 volatile int32_t C[n_len * p_len];
 volatile int32_t C_ref[n_len * p_len];
@@ -34,41 +34,37 @@ volatile int32_t C_ref[n_len * p_len];
   // 8-bit matrix mult
   #if defined(USE_PEXT)   // n, m and p must be a multiple of 4
     // C = AB with A = [n x m], B = [m x p], C = [n x p]
-    void matmul(int32_t* c, const int8_t* a, const int8_t* b,
-                    const unsigned long int n, const unsigned long int m,
-                    const unsigned long int p) {
+    void matmul(int32_t* c, const int8_t* a, const int8_t* b) {
 
       // Need to tranpose b first
       // b_T is of shape p*m
-      int8_t b_T[p * m];
-      for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < p; ++j) {
-          b_T[j * m + i] = b[i * p + j];
+      int8_t b_T[p_len * m_len];
+      for (int i = 0; i < m_len; ++i) {
+        for (int j = 0; j < p_len; ++j) {
+          b_T[j * m_len + i] = b[i * p_len + j];
         }
       }
 
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < p; ++j) {
-          int sum = 0;
-          for (int k = 0; k < m/4; ++k) {       // I know this is ugly, but its very efficient
-            sum = __rv_smaqa(sum, *((int *)(&a[i * n + k * 4])), *((int *)(&b_T[j * p  + k * 4])));
+      for (int i = 0; i < n_len; i++) {
+        for (int j = 0; j < p_len; j++) {
+          int32_t sum = 0;
+          for (int k = 0; k < m_len; k+=4) {       // I know this is ugly, but its very efficient
+            sum = __rv_smaqa(sum, *((int32_t *)(&a[i * n_len + k])), *((int32_t *)(&b_T[j * p_len  + k])));
           }
-          c[i * n + j] = sum;
+          c[i * p_len + j] = sum;
         }
       }
     }
   #else
     // C = AB with A = [n x m], B = [m x p], C = [n x p]
-    void matmul(int32_t* c, const int8_t* a, const int8_t* b,
-                    const unsigned long int n, const unsigned long int m,
-                    const unsigned long int p) {
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < p; ++j) {
+    void matmul(int32_t* c, const int8_t* a, const int8_t* b) {
+      for (int i = 0; i < n_len; ++i) {
+        for (int j = 0; j < p_len; ++j) {
           int32_t sum = 0;
-          for (int k = 0; k < m; ++k) {
-            sum += a[i * m + k] * b[k * p + j];
+          for (int k = 0; k < m_len; ++k) {
+            sum += a[i * m_len + k] * b[k * p_len + j];
           }
-          c[i * p + j] = sum;
+          c[i * p_len + j] = sum;
         }
       }
     }
@@ -77,41 +73,37 @@ volatile int32_t C_ref[n_len * p_len];
   // 16-bit matrix mult
   #if defined(USE_PEXT)
     // C = AB with A = [n x m], B = [m x p], C = [n x p]
-    void matmul(int32_t* c, const int16_t* a, const int16_t* b,
-                    const unsigned long int n, const unsigned long int m,
-                    const unsigned long int p) {
+    void matmul(int32_t* c, const int16_t* a, const int16_t* b) {
 
       // Need to tranpose b first
       // b_T is of shape p*m
-      int16_t b_T[p * m] __attribute__ ((aligned(8)));
-      for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < p; ++j) {
-          b_T[j * m + i] = b[i * p + j];
+      int16_t b_T[p_len * m_len] __attribute__ ((aligned(8)));
+      for (int i = 0; i < m_len; ++i) {
+        for (int j = 0; j < p_len; ++j) {
+          b_T[j * m_len + i] = b[i * p_len + j];
         }
       }
 
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < p; ++j) {
+      for (int i = 0; i < n_len; ++i) {
+        for (int j = 0; j < p_len; ++j) {
           int sum = 0;
-          for (int k = 0; k < m/2; ++k) {       // I know this is ugly, but its very efficient
-            sum = __rv_kmada(sum, *((int *)(&a[i * n + k * 2])), *((int *)(&b_T[j * p  + k * 2])));
+          for (int k = 0; k < m_len; k += 2) {       // I know this is ugly, but its very efficient
+            sum = __rv_kmada(sum, *((int *)(&a[i * n_len + k])), *((int *)(&b_T[j * p_len  + k])));
           }
-          c[i * n + j] = sum;
+          c[i * n_len + j] = sum;
         }
       }
     }
   #else
     // C = AB with A = [n x m], B = [m x p], C = [n x p]
-    void matmul(int32_t* c, const int16_t* a, const int16_t* b,
-                    const unsigned long int n, const unsigned long int m,
-                    const unsigned long int p) {
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < p; ++j) {
+    void matmul(int32_t* c, const int16_t* a, const int16_t* b) {
+      for (int i = 0; i < n_len; ++i) {
+        for (int j = 0; j < p_len; ++j) {
           int32_t sum = 0;
-          for (int k = 0; k < m; ++k) {
-            sum += a[i * m + k] * b[k * p + j];
+          for (int k = 0; k < m_len; ++k) {
+            sum += a[i * m_len + k] * b[k * p_len + j];
           }
-          c[i * p + j] = sum;
+          c[i * p_len + j] = sum;
         }
       }
     }
@@ -120,31 +112,27 @@ volatile int32_t C_ref[n_len * p_len];
   // 32-bit matrix mult
   #if defined(USE_PEXT)
     // C = AB with A = [n x m], B = [m x p], C = [n x p]
-    void matmul(int32_t* c, const int32_t* a, const int32_t* b,
-                    const unsigned long int n, const unsigned long int m,
-                    const unsigned long int p) {
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < p; ++j) {
+    void matmul(int32_t *c, const int32_t *a, const int32_t *b) {
+      for (int i = 0; i < n_len; ++i) {
+        for (int j = 0; j < p_len; ++j) {
           int32_t sum = 0;
-          for (int k = 0; k < m; ++k) {
-            sum = __rv_smar32(sum, a[i * m + k] * b[k * p + j]);
+          for (int k = 0; k < m_len; ++k) {
+            sum += a[i * m_len + k] * b[k * p_len + j];
           }
-          c[i * p + j] = sum;
+          c[i * p_len + j] = sum;
         }
       }
     }
   #else
     // C = AB with A = [n x m], B = [m x p], C = [n x p]
-    void matmul(int32_t* c, const int32_t* a, const int32_t* b,
-                    const unsigned long int n, const unsigned long int m,
-                    const unsigned long int p) {
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < p; ++j) {
+    void matmul(int32_t* c, const int32_t* a, const int32_t* b) {
+      for (int i = 0; i < n_len; ++i) {
+        for (int j = 0; j < p_len; ++j) {
           int32_t sum = 0;
-          for (int k = 0; k < m; ++k) {
-            sum += a[i * m + k] * b[k * p + j];
+          for (int k = 0; k < m_len; ++k) {
+            sum += a[i * m_len + k] * b[k * p_len + j];
           }
-          c[i * p + j] = sum;
+          c[i * p_len + j] = sum;
         }
       }
     }
@@ -177,15 +165,15 @@ int main(int argc, char **argv) {
   //print_matrix_word(&A, n_len, m_len);
   //print_matrix_word(&B, m_len, p_len);
 
-  puts("Generating reference result...\n");
+  //puts("Generating reference result...\n");
   //matmul_ref(&C_ref, &A, &B, n_len, m_len, p_len);
 
-  puts("Running MatMult benchmark...\n");
+  //puts("Running MatMult benchmark...\n");
   pcount_enable(1);
-  matmul(&C, &A, &B, n_len, m_len, p_len);
+  matmul(&C, &A, &B);
   pcount_enable(0);
 
-  puts("Result: \n");
+  //puts("Result: \n");
   //is_equal(&C, &C_ref, n_len, p_len);
   //print_matrix_word(&C, n_len, p_len);
   //print_matrix_word(&C_ref, n_len, p_len);
