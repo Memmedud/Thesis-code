d4d9b649e598afd53bb6c43ad9e802ba8161d88c
Mathias Støle <mathst23@gmail.com>
2024-04-23 14:52:22 +0200

    Update testbench

--- hw/pext_tb/tb/ibex_pext_tb.sv
+++ hw/pext_tb/tb/ibex_pext_tb.sv
@@ -18,6 +18,7 @@ module ibex_pext_tb;
   logic[31:0] alu_result;
 
   ibex_pkg_pext::zpn_op_e     zpn_operator;
+  logic                       zpn_instr;
   ibex_pkg::alu_op_e          alu_operator;
   ibex_pkg::md_op_e           md_operator;
 
@@ -50,7 +51,7 @@ module ibex_pext_tb;
     .rst_ni             (1'b1),
 
     .zpn_operator_i     (zpn_operator),
-    .zpn_instr_i        (1'b1),
+    .zpn_instr_i        (zpn_instr),
     .alu_operator_i     (alu_operator),
     .multdiv_operator_i (md_operator),
 
@@ -84,56 +85,50 @@ module ibex_pext_tb;
 
   always @(posedge clk) begin
     //$display($stime,,,"alu_result=%8h mult_valid=%1b imd_val1=%8h imd_val0=%8h quad=%2b cc=%2b div_en=%1b divbyzero=%1b divstate=%3b", alu_result, valid, imd_val_q[1], imd_val_q[0], alu_pext.mult_pext_i.zpn_signed_mult, alu_pext.mult_pext_i.cycle_count, alu_pext.mult_pext_i.div_en_internal, alu_pext.mult_pext_i.div_by_zero_d, alu_pext.mult_pext_i.md_state_q);
-    $display($stime,,,"alu_result=%8h mult_result=%12h imd_val1=%8h imd_val0=%8h op_b=%8h valid=%1b is_byte_less=%4b signed_mult=%2b", alu_result, alu_pext.mult_pext_i.mult_sum_16x16_0, alu_pext.mult_pext_i.mult_sum_16x16_1, imd_val_d[1], imd_val_d[0], alu_pext.mult_pext_i.valid_o, alu_pext.is_byte_less, alu_pext.mult_pext_i.zpn_signed_mult);
+    $display($stime,,,"alu_result=%8h valid=%8h r1=%8h r2=%8h r3=%8h a_sign=%3b, b_sign=%3b state=%2b imd=%9h", alu_result, imd_val_d[0], alu_pext.mult_pext_i.summand_LL, alu_pext.mult_pext_i.summand_HL, alu_pext.mult_pext_i.mult3_res, alu_pext.mult_pext_i.op_a_signs, alu_pext.mult_pext_i.op_b_signs, alu_pext.mult_pext_i.mult_state_q, imd_val_q[0]);
   end
 
   initial begin
     #20;
 
-    imm_val = 5'h1b;
+    imm_val = 5'h0;
 
-    //alu_operand_a  = 32'h1545_0015;
-    //alu_operand_a  = 32'hffff_7fff;
-    //alu_operand_a  = 32'haaaa_aaab;
-    alu_operand_a  = 32'hff7fffff;
-    //alu_operand_a  = 32'hffff_fdff;
-    //alu_operand_b  = 32'h5142_18d4;
-    //alu_operand_b  = 32'hffff_f7ff;
-    //alu_operand_b  = 32'hffff_ffff;
-    alu_operand_b  = 32'h8;
-    //alu_operand_b  = 32'h0000_0003;
-    alu_operand_rd = 32'h0;
 
-    zpn_operator = ZPN_KSLLW;
+    //alu_operand_a  = 32'hfffff7ff;
+    alu_operand_a  = 32'hfffff7ff;
+
+    //alu_operand_b  = 32'hffff7fff;
+    alu_operand_b  = 32'h7fffffbf;
+    alu_operand_rd = 32'h10;
+
+    zpn_operator = ZPN_SMMWB;
     alu_operator = ZPN_INSTR;
     md_operator  = MD_OP_MULL;
 
-    multdiv_sel = 1'b0;
-    mult_en = 1'b0;
+    multdiv_sel = 1'b1;
+    mult_en = 1'b1;
     div_en = 1'b0;
     mult_sel = mult_en;
     div_sel = div_en;
-    signed_mode = 2'b00;
+    signed_mode = 2'b11;
     multdiv_ready_id = 1'b0;
     data_ind_timing = 1'b0;
+    zpn_instr = (alu_operator == ZPN_INSTR);
 
-    #10;
+    #40;
     //mult_en = 1'b1;
     //$display("%4b", alu_pext.saturated);
-    $display("%8h", ~alu_pext.shift_mask);
-    $display("%4b", alu_pext.saturation_bytes);
-    $display("%4b", alu_pext.shift_saturation);
-    $display("%4b", alu_pext.saturated);
-    $display("%4b", alu_pext.shift);
-    $display("%8h", alu_pext.operand_a_i);
-    $display("%8h", alu_pext.shift_left);
-    $display("%8h", alu_pext.shift_result);
-    $display("%8h", alu_pext.saturating_result);
-    $display("%8h", alu_pext.sat_imm_shift_result);
-    $display("%8h", alu_pext.sat_op2);
-    $display("%8h", alu_pext.sat_op1);
-    $display("%8h", alu_pext.imm_instr);
 
+  
+    $display("%8h", alu_pext.shift_left);
+    $display("%8h", alu_pext.shift_result_rev);
+    $display("%8h", alu_pext.shift_amt);
+    $display("%8h", alu_pext.width32);
+    $display("%8h", alu_pext.normal_result);
+    $display("%8h", alu_pext.oneop);
+    $display("%8h", alu_pext.sub);
+    $display("%8h", alu_pext.multdiv_operand_a);
+    $display("%8h", alu_pext.multdiv_operand_b);
 
     #40;
     //#500;
